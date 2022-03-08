aa76118e10320eb9db697606c9adff53ee6f3776
Vagesh Vora <vageshvora12@gmail.com>
2022-03-08 20:59:54 +0530

    app.cors

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ app.use(session({
     resave:true,
     saveUninitialized:true
 }))
-app.use(cors({credentials:true,origin:"https://ecomm-a.herokuapp.com/"}));
+// app.use(cors({credentials:true,origin:"https://ecomm-a.herokuapp.com/"}));
+app.use(cors(corsOptions));
+// app.use(cors);
 app.use(cookieParser());
 app.use('/user', userRoute);
 app.use('/category',categoryRoute);
