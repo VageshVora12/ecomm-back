9efe2a3dc616848e0093482e6c79c1f1ea40876e
Vagesh Vora <vageshvora12@gmail.com>
2022-03-08 19:58:02 +0530

    post get

--- Route/CategoryRoute.js
+++ Route/CategoryRoute.js
@@ -2,5 +2,5 @@ import express from "express";
 import {CategoryMain,CategoryList} from '../Controller/CategoryController';
 const categoryRoute = express.Router();
 categoryRoute.post("/category-main",CategoryMain);
-categoryRoute.post("/category-list",CategoryList)
+categoryRoute.get("/category-list",CategoryList)
 export default categoryRoute;  
--- Route/ProductRoute.js
+++ Route/ProductRoute.js
@@ -2,5 +2,5 @@ import express from "express";
 import {ProductMain,ProductList} from '../Controller/ProductController';
 const productRoute = express.Router();
 productRoute.post("/product-main",ProductMain);
-productRoute.post("/product-list",ProductList);
+productRoute.get("/product-list",ProductList);
 export default productRoute;  
